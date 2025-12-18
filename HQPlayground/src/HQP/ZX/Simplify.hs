e70cdc9049a1238db8946573ac072c20919c2e7b
Jens Kristensen <jens-nk@hotmail.com>
2025-12-18 10:12:46 +0100

    add suggestions to new simplification rules

--- HQPlayground/src/HQP/ZX/Simplify.hs
+++ HQPlayground/src/HQP/ZX/Simplify.hs
@@ -21,3 +21,15 @@ mergeRedsRec (redSpider:redSpiders) g =
                 newSpider = Node v (Red (newPhase))
                 newGraph = removeEdge newSpider newSpider $ mergeVertices (\x -> x `elem` verticesToMerge) newSpider g
             in mergeRedsRec (newSpider:nonAdjacentReds) newGraph
+
+-- Same as merge red spiders?? Special case with only two neighbors for each Hadamard.
+-- hadamardRule :: ZXDiagram -> ZXDiagram
+
+
+
+-- If a spider has a majority of Hadamard gates on its legs, change its color??
+-- changeColorOnMajority :: ZXDiagram -> ZXDiagram
+
+
+-- If a spider has phase 0 and two legs, it can be removed. - What about multiple legs?
+-- remove0PhaseSpiders :: ZXDiagram -> ZXDiagram
